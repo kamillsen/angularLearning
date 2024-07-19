aeae01022bb5949eadbdd10f3b824b025c6d625b
kamillsen <senkamil1864mail.com>
2024-07-19 17:05:15 +0300

    version-2 için dosyada temizleme yaptım

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,45 +12,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title:string = 'this loaded dynamically' ;
-  imgURL:string = 'https://d585tldpucybw.cloudfront.net/sfimages/default-source/blogs/2023/2023-11/angular-logo-1200-628.png?sfvrsn=bf64b1ee_3'
-
-  isDisable: boolean = true ;
-
-  isActivite:boolean = true;
-
-  fruitName:string = 'apple'
-
-  userName:string = 'John Doe'
-$kamil: any;
-
-  buttosnClick(){
-    console.log('Button Click');
-  }
-
-
-  keyEnter(event:any) {
-                                //* enter keyCode = 13
-    console.log(event.keyCode); //* burda evet yerine başka bir isim yazabiliriz o sadece bir 
-
-    if(event.keyCode == 13 ) {
-      console.log('Enter Key Pressed');
-    }
-  }
-
-  keyupFiltering(user:HTMLInputElement) {
-    console.log(user.value);
-  }
-
-  updateUserName(username:HTMLInputElement) {
-    this.userName = username.value
-    console.log(this.userName);
-  }
-
-  onKeyup(){
-    console.log(this.userName)
-
-  }
+ 
  
 }
 
