f8cb9f12937a8edabf7106d820ca26733ee0bfc6
kamillsen <senkamil1864mail.com>
2024-07-21 17:19:51 +0300

    version-2 den kalanlar temizlendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,55 +14,7 @@ import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 })
 export class AppComponent {
  
-    isLoggedIn:boolean = true;
-    userName:string = 'Kamil ŞEN';
-
-    isAdmin:boolean = false;
-    isMember:boolean = false;
-    isGuest:boolean = true;
-
-    loginCount:number=0;
-
-    userRole:string="Member";
-
-    countLogginAttempts(){
-      this.loginCount ++;
-      console.log(this.loginCount);
-    }
-
-
-    /* 
-    * Ng For Directive Part 9
-    */
-
-    users:Array<string> = ['John','Sam','Smith','Raj'];
-
-    usersObj: Array<any> = [
-      { id: 1, name:'Talha', email: 'talha@gmail.com'},
-      { id: 2, name:'Kamil', email: 'kamil@gmail.com'},
-      { id: 3, name:'Caner', email: 'caner@gmail.com'},
-      { id: 4, name:'Semih', email: 'semih@gmail.com'}
-    ]
-
-    constructor(){
-      console.log(this.usersObj.length);
-    }
-
-    addNewUser(){
-      let user = {id:5 , name: 'User 1', email: 'user1@gmail.com'};
-      this.usersObj.push(user);
-    }
-
-    onDelete1(user:object){
-        let index=this.usersObj.indexOf(user);
-        console.log(index);
-        this.usersObj.splice(index, 1) //* index ten başla 1 adet öge sil
-    }
-
-    onDelete2(index:number){
-     this.usersObj.splice(index,1)
-  }
- 
+   
 }
 
 
