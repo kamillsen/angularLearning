098e3d04df1902175cf613cdd87002871e8ef5ab
kamillsen <senkamil1864mail.com>
2024-07-21 20:05:09 +0300

    version-3 part 12 finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,35 @@ import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AppNavbar,HeaderComponent,FormsModule,NgIf,NgTemplateOutlet,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
+  imports: [
+    RouterOutlet,
+    AppNavbar,
+    HeaderComponent,
+    FormsModule,
+    NgIf,
+    NgTemplateOutlet,
+    NgFor,NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgStyle,
+   NgClass,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
  
    userRole:string='Admin';
+
+   isLoggedIn:boolean=true;
 }
+
+
+
 
 
